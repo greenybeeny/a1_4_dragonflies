58dbd25deff65ca6156a65f6b37747145f765d5e
greenybeeny <92817241+greenybeeny@users.noreply.github.com>
2021-10-27 14:13:13 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.showNumber(5)
+basic.pause(1000)
+// show number 4
+basic.showNumber(4)
+basic.pause(1000)
+basic.showNumber(3)
+// pause for 1000 ms or 1 second
+basic.pause(1000)
+// show number 2
+basic.showNumber(2)
+// pause for 1000 ms or 1 second
+basic.pause(1000)
+// show number 1
+basic.showNumber(1)
+// pause for 1000 ms or 1 second
+basic.pause(1000)
+basic.showNumber(0)
+basic.showString("Happy New Year!")
